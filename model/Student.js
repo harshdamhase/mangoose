b023613a9690f5a557a71b1af48a4649e263f41d
Harshda Mhase <harshadamhase12@gmail.com>
2023-06-08 09:41:37 -0900

    doc: mongoose-lib-added

---
+++ model/Student.js
@@ -0,0 +1,11 @@
+import {Schema,model} from "mongoose";
+
+const studentSchema = new Schema({
+    fullname : String,
+    email: String,
+    regNo : Number
+});
+
+const Student = model('Student', studentSchema);
+
+export default Student;
